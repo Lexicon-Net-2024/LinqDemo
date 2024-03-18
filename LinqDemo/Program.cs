b580346869cddd7d42b1313c98e71caeecb65d16
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-03-18 11:12:41 +0100

    Add project files.

---
+++ LinqDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LinqDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = GetPersons();
+        }
+
+        private static IEnumerable<Person> GetPersons()
+        {
+            return new List<Person>
+                {
+                new("Nisse", 20),
+                new("Nisse", 21),
+                new("Nisse", 22),
+                new("Nisse", 23),
+                new("Nisse", 24),
+                new("Nisse", 24),
+                new("Nisse", 26),
+                new("Pelle" ,65),
+                new("Pelle" ,60),
+                new("Pelle" ,62),
+                new("Olle" , 66),
+                new("Sara" , 54),
+                new("Ida" ,  36),
+                new("Fia",   45),
+                new("Lisa",   45),
+                new("Sophia-Magdalena", 32),
+            };
+        }
+    }
+}
